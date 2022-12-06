03415d079eb72086f4e13e25e8b8114a1efed8f3
ismolin <ilya.smolin1996@yandex.ru>
2022-12-06 15:30:35 +0300

    some changes in names of variables

--- buttons.py
+++ buttons.py
@@ -1,5 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
+############################### user keyboard ##########################
+
 new_words = KeyboardButton('Учить новые слова')
 repeat_words = KeyboardButton('Повторить слова')
 settings = KeyboardButton('Настройки')
@@ -7,60 +9,57 @@ settings = KeyboardButton('Настройки')
 user_keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 user_keyboard.add(new_words).add(repeat_words)
 
-########################################################################
+########################### word count keyboard ########################
 
-button3 = KeyboardButton('5')
-button4 = KeyboardButton('10')
-button5 = KeyboardButton('15')
-button6 = KeyboardButton('20')
+button5 = KeyboardButton('5')
+button10 = KeyboardButton('10')
+button15 = KeyboardButton('15')
+button20 = KeyboardButton('20')
 
 word_count_keyboard = ReplyKeyboardMarkup()
-word_count_keyboard.add(button3, button4).add(button5, button6)
+word_count_keyboard.add(button5, button10).add(button15, button20)
 
-########################################################################
+##################### time repeat keyboard #############################
 
 time_repeat_keyboard = ReplyKeyboardMarkup()
 time_repeat_keyboard.add(*[KeyboardButton(f'{x:02}:00') for x in range(24)]).add(KeyboardButton('Не надо '
                                                                                                 'мне напоминать'))
 
-########################################################################
+##################### categories keyboard #############################
 
-button32 = KeyboardButton('1000 самых употребляемых слов')
-button33 = KeyboardButton('5000 самых употребляемых слов')
-button34 = KeyboardButton('Слова для IT')
-button35 = KeyboardButton('Продолжить')
+button_1000 = KeyboardButton('1000 самых употребляемых слов')
+button_5000 = KeyboardButton('5000 самых употребляемых слов')
+button_IT = KeyboardButton('Слова для IT')
+button_continue = KeyboardButton('Продолжить')
 
 categories_keyboard = ReplyKeyboardMarkup()
 categories_keyboard_with_next_button = ReplyKeyboardMarkup()
-categories_keyboard.add(button32).add(button33).add(button34)
-categories_keyboard_with_next_button.add(button32).add(button33).add(button34).add(button35)
+categories_keyboard.add(button_1000).add(button_5000).add(button_IT)
+categories_keyboard_with_next_button.add(button_1000).add(button_5000).add(button_IT).add(button_continue)
 
-########################################################################
+##################### new words quizlet keyboard #############################
 
-button36 = InlineKeyboardButton('Я уже знаю это слово', callback_data='Я уже знаю это слово')
-button37 = InlineKeyboardButton('Начать учить это слово', callback_data='Начать учить это слово')
+button_known_word = InlineKeyboardButton('Я уже знаю это слово', callback_data='Я уже знаю это слово')
+button_unknown_word = InlineKeyboardButton('Начать учить это слово', callback_data='Начать учить это слово')
 
 new_words_quizlet_keyboard = InlineKeyboardMarkup()
-new_words_quizlet_keyboard.add(button36).add(button37)
+new_words_quizlet_keyboard.add(button_known_word).add(button_unknown_word)
 
-########################################################################
-
-button38 = InlineKeyboardButton('Я запомнил, отложить для повтора', callback_data='Я запомнил, отложить для повтора')
-button39 = InlineKeyboardButton('Показывать это слово еще', callback_data='Показывать это слово еще')
+button_next = InlineKeyboardButton('Я запомнил, отложить для повтора', callback_data='Я запомнил, отложить для повтора')
+button_try = InlineKeyboardButton('Показывать это слово еще', callback_data='Показывать это слово еще')
 
 new_words_quizlet_keyboard2 = InlineKeyboardMarkup()
-new_words_quizlet_keyboard2.add(button38).add(button39)
+new_words_quizlet_keyboard2.add(button_next).add(button_try)
 
-########################################################################
+##################### repetition words keyboard #############################
 
-button40 = InlineKeyboardButton('Я вспомнил это слово', callback_data='Я вспомнил это слово')
-button41 = InlineKeyboardButton('Не вспомнил', callback_data='Не вспомнил')
+button_yes = InlineKeyboardButton('Я вспомнил это слово', callback_data='Я вспомнил это слово')
+button_nope = InlineKeyboardButton('Не вспомнил', callback_data='Не вспомнил')
 
 repetition_words_keyboard = InlineKeyboardMarkup()
-repetition_words_keyboard.add(button40).add(button41)
+repetition_words_keyboard.add(button_yes).add(button_nope)
 
-########################################################################
-
+##################### general menu button #############################
 
 buttonGeneralMenuButton = KeyboardButton('Завершить')
 
