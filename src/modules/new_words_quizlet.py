a96521d371dc1645882bf3a96631c6412f837b2f
ismo <ilya.smolin1996@yandex.ru>
2025-06-16 18:04:42 +0300

    changed imports, connections to db

--- src/modules/new_words_quizlet.py
+++ src/modules/new_words_quizlet.py
@@ -1,5 +1,4 @@
-from src.database.connections import db_select, db_update, db_update_many
-from aiogram.utils.markdown import hspoiler
+from src.database.connections import db
 from src.content.buttons import new_words_quizlet_keyboard, new_words_quizlet_keyboard2, user_keyboard
 import random
 import datetime
@@ -21,16 +20,16 @@ class NewWordsQuizlet:
     async def create_quantity_of_words_from_category_list(self):
         """Creating a list of new words to study in the database"""
 
-        await db_update(f"""TRUNCATE TABLE {self.user_name}_days_words_list""")
+        await db.execute_void(query=f"""TRUNCATE TABLE {self.user_name}_days_words_list""")
 
         # Extracting the number of words per day, the number of user categories,
         # and the average number of words to take from each category
 
-        total_quantity_of_words_query = await db_select(sql=f"""SELECT total_quantity_of_words                           
+        total_quantity_of_words_query = await db.execute_one(query=f"""SELECT total_quantity_of_words                           
                                                                 FROM {self.user_name}_info
                                                                 WHERE total_quantity_of_words IS NOT NULL""")
         total_quantity_of_words = int(total_quantity_of_words_query[0][0])
-        quantity_of_categories_query = await db_select(sql=f"""SELECT COUNT(categories) 
+        quantity_of_categories_query = await db.execute_one(query=f"""SELECT COUNT(categories) 
                                                                FROM {self.user_name}_info""")
         quantity_of_categories = int(quantity_of_categories_query[0][0])
         average_quantity_words = int(total_quantity_of_words / quantity_of_categories)
@@ -60,19 +59,19 @@ class NewWordsQuizlet:
         """Creating a list of new words for the user and inserting it into the database"""
 
         for categories, local_count_of_words in zip(categories_list, quantity_of_words_from_category_list):
-            words = await db_select(f"""SELECT {categories}.words_eng, {categories}.words_rus
+            words = await db.execute_one(f"""SELECT {categories}.words_eng, {categories}.words_rus
                                         FROM {categories}
                                         WHERE {categories}.words_eng NOT IN
                                         (SELECT words_eng FROM {self.user_name}_words)
                                         ORDER BY RANDOM()
                                         LIMIT {local_count_of_words}""")
-            await db_update_many(f"""INSERT INTO {self.user_name}_days_words_list(words_eng, words_rus) 
-                                     VALUES %s""", words)
+            await db.execute_many(f"""INSERT INTO {self.user_name}_days_words_list(words_eng, words_rus) 
+                                     VALUES ($1, $2)""", words)
 
     async def create_categories_list(self):
         """Generating a list with user categories"""
 
-        categories_import = await db_select(sql=f"""SELECT categories 
+        categories_import = await db.execute_one(query=f"""SELECT categories 
                                                     FROM {self.user_name}_info 
                                                     WHERE categories not in ('None')""")
         categories_list = []
@@ -87,30 +86,30 @@ class NewWordsQuizlet:
 
     async def create_quizlet_card(self):
         """Generating text for a quizlet card"""
-        result = await db_select(f'''SELECT words_eng, words_rus 
+        result = await db.execute_one(f'''SELECT words_eng, words_rus 
                                      FROM {self.user_name}_days_words_list 
                                      LIMIT 1''')
-        spoiler = hspoiler(result[0][1])
+        spoiler = f'||{result[0][1]}||'
         return result[0][0] + '\n' + f'{spoiler}'
 
     async def generate_new_word(self, categories):
         """This method generates one random new word and adds it to the list of words to study"""
 
         category = random.choice(categories)
-        new_word = await db_select(sql=f'''SELECT words_eng, words_rus FROM {category} WHERE words_eng NOT IN 
+        new_word = await db.execute_one(query=f'''SELECT words_eng, words_rus FROM {category} WHERE words_eng NOT IN 
                                            (SELECT words_eng from {self.user_name}_days_words_list
                                            UNION 
                                            SELECT words_eng from {self.user_name}_words) 
                                            ORDER BY RANDOM()
                                            LIMIT 1''')
-        await db_update(sql=f"""INSERT INTO {self.user_name}_days_words_list(words_eng, words_rus) 
+        await db.execute_void(query=f"""INSERT INTO {self.user_name}_days_words_list(words_eng, words_rus) 
                                 VALUES ('{new_word[0][0]}','{new_word[0][1]}')""")
 
     async def i_know_this_word(self):
         """ This method removes a word from the quizlet list and adds the word to the user's list
                                              with the flag "6" - "known" """
 
-        await db_update(sql=f"""DELETE FROM {self.user_name}_days_words_list 
+        await db.execute_void(query=f"""DELETE FROM {self.user_name}_days_words_list 
                                 WHERE words_eng = '{self.word_en}';
                                 INSERT INTO {self.user_name}_words
                                 (words_eng, words_rus, words_count) 
@@ -121,7 +120,7 @@ class NewWordsQuizlet:
 
         time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        await db_update(sql=f"""DELETE FROM {self.user_name}_days_words_list 
+        await db.execute_void(query=f"""DELETE FROM {self.user_name}_days_words_list 
                                 WHERE words_eng = '{self.word_en}';
                                 INSERT INTO {self.user_name}_days_words_list(words_eng, words_rus) 
                                 VALUES ('{self.word_en}','{self.word_ru}');
@@ -139,7 +138,7 @@ class NewWordsQuizlet:
         else:
             await self.bot.delete_message(self.user_id, self.message_id)
             word_en = next_word_card.split('\n')[0]
-            if len(await db_select(sql=f"""SELECT * FROM {self.user_name}_words 
+            if len(await db.execute_one(query=f"""SELECT * FROM {self.user_name}_words 
                                            WHERE words_eng = '{word_en}'""")) > 0:
                 await self.bot.send_message(self.user_id, next_word_card, parse_mode='HTML',
                                             reply_markup=new_words_quizlet_keyboard2)
@@ -151,13 +150,13 @@ class NewWordsQuizlet:
     async def delete_from_words_list(self):
         """ This method removes a word from the user's list """
 
-        await db_update(sql=f"""DELETE FROM {self.user_name}_days_words_list 
+        await db.execute_void(query=f"""DELETE FROM {self.user_name}_days_words_list 
                                 WHERE words_eng = '{self.word_en}'""")
 
     async def this_is_last_word(self):
         """ This method checks if the word is the last one in the list """
 
-        return len(await db_select(sql=f"""SELECT * FROM {self.user_name}_days_words_list""")) == 1
+        return len(await db.execute_one(query=f"""SELECT * FROM {self.user_name}_days_words_list""")) == 1
 
     async def the_end(self):
         """ This method completes the learning of new words """
@@ -169,7 +168,7 @@ class NewWordsQuizlet:
     async def show_this_word_again(self):
         """ This method moves a word to last position in user's list """
 
-        await db_update(sql=f"""DELETE FROM {self.user_name}_days_words_list 
+        await db.execute_void(query=f"""DELETE FROM {self.user_name}_days_words_list 
                                 WHERE words_eng = '{self.word_en}';
                                 INSERT INTO {self.user_name}_days_words_list(words_eng, words_rus) 
                                 VALUES ('{self.word_en}','{self.word_ru}')""")
