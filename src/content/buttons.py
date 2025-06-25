604631afc4acf54597dcefd1558d12b2813fa423
ismo <ilya.smolin1996@yandex.ru>
2025-06-25 18:17:58 +0300

    some changes in keyboards

--- src/content/buttons.py
+++ src/content/buttons.py
@@ -1,34 +1,29 @@
 from aiogram.types import (
     ReplyKeyboardMarkup,
     KeyboardButton,
+    InlineKeyboardMarkup,
     InlineKeyboardButton,
-    InlineKeyboardMarkup
 )
 
-############################### user keyboard ##########################
-
+# ==== Главное меню пользователя ====
 user_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
     [KeyboardButton(text='Учить новые слова')],
     [KeyboardButton(text='Повторить слова')],
     [KeyboardButton(text='Настройки')],
 ])
 
-########################### word count keyboard ########################
-
+# ==== Выбор количества слов ====
 word_count_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
     [KeyboardButton(text='5'), KeyboardButton(text='10')],
     [KeyboardButton(text='15'), KeyboardButton(text='20')],
 ])
 
-##################### time repeat keyboard #############################
-
+# ==== Время уведомлений ====
 time_buttons = [[KeyboardButton(text=f"{hour:02}:00")] for hour in range(24)]
 time_buttons.append([KeyboardButton(text='Не надо мне напоминать')])
-
 time_repeat_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=time_buttons)
 
-##################### categories keyboard #############################
-
+# ==== Категории ====
 categories_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
     [KeyboardButton(text='1000 самых употребляемых слов')],
     [KeyboardButton(text='5000 самых употребляемых слов')],
@@ -42,8 +37,20 @@ categories_keyboard_with_next_button = ReplyKeyboardMarkup(resize_keyboard=True,
     [KeyboardButton(text='Продолжить')],
 ])
 
-##################### new words quizlet keyboard #############################
+# ==== Меню настроек ====
+settings_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+    [KeyboardButton(text='Изменить количество слов в день')],
+    [KeyboardButton(text='Изменить категории')],
+    [KeyboardButton(text='Настройки уведомлений')],
+    [KeyboardButton(text='Главное меню')],
+])
 
+# ==== Завершение ====
+general_menu_button = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
+    [KeyboardButton(text='Завершить')],
+])
+
+# ==== Inline keyboards ====
 new_words_quizlet_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Я уже знаю это слово', callback_data='Я уже знаю это слово')],
     [InlineKeyboardButton(text='Начать учить это слово', callback_data='Начать учить это слово')],
@@ -54,29 +61,10 @@ new_words_quizlet_keyboard2 = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Показывать это слово еще', callback_data='Показывать это слово еще')],
 ])
 
-##################### repetition words keyboard #############################
-
 repetition_words_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Я вспомнил это слово', callback_data='Я вспомнил это слово')],
     [InlineKeyboardButton(text='Не вспомнил', callback_data='Не вспомнил')],
 ])
-
-##################### general menu button #############################
-
-general_menu_button = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
-    [KeyboardButton(text='Завершить')],
-])
-
-##################### settings keyboard #############################
-
-settings_keyboard = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=[
-    [KeyboardButton(text='Изменить количество слов в день')],
-    [KeyboardButton(text='Изменить категории')],
-    [KeyboardButton(text='Настройки уведомлений')],
-    [KeyboardButton(text='Главное меню')],
-])
-
-##################### add user words keyboard #############################
 
 add_user_words_keyboard = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='Изменить карточку', callback_data='Изменить перевод')],
